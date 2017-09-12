3038b8ce3f8339cd083166774f367f823130d21a
Soumyajit Gorai <31511160+soumya173@users.noreply.github.com>
2017-09-12 17:04:50 +0530

    Final Changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('#alertMe').click(function(e){
+		e.preventDefault();
+		$('#successAlert').slideDown();
+	});
+	
+	$("a.pop").click(function(e){
+		e.preventDefault();
+		$("")
+	});
+	
+	$("a.pop").popover();
+	
+	$('[rel="tooltip"]').tooltip();
+});
